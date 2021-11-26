73c87500c4673db400f2760059aea27de7e08468
Zoky-2020 <wzq_2020@126.com>
2021-11-26 14:02:59 +0800

    update

---
+++ scripts/train_vit_base.sh
@@ -0,0 +1,1 @@
+python train.py --config_file configs/Market/vit_base.yml MODEL.DEVICE_ID "('0')";
---
+++ scripts/train_vit_transreid.sh
@@ -0,0 +1,1 @@
+python train.py --config_file configs/Market/vit_transreid.yml MODEL.DEVICE_ID "('0')";
